06fbea5e4e1128e928351b040d29a1fe7d92e4fc
Neel Bhavsar <neel.bhavsar@artoon.in>
2026-03-17 00:28:01 +0530

    fix : cors error fixed

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -18,7 +18,9 @@ app.use(helmet());
 // CORS
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+        origin: process.env.CORS_ORIGIN
+            ? process.env.CORS_ORIGIN.split(',')
+            : true, // True reflects the request origin
         credentials: true,
         methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization'],
